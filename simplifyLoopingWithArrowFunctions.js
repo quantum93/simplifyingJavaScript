9f62eb725245d7e432e560a1989714d3cf4825bc
Tae Lee <taebumlee@gmail.com>
2020-09-09 18:48:27 -0700

    Update simplifyLoopingWithArrowFunctions.js

--- simplifyLoopingWithArrowFunctions.js
+++ simplifyLoopingWithArrowFunctions.js
@@ -1,1 +1,7 @@
-const formatUser = name => `${capital}`
+/** fat arrow function에서 함수의 바디가 한 줄일 경우 
+ * return과 { }까지도 생략이 가능하다. 
+ */
+
+const capitalize = name => name[0].toUpperCase() + name.slice(1);
+const formatUser = name => `${capitalize(name)} is logged in`;
+console.log(formatUser("tae"));
