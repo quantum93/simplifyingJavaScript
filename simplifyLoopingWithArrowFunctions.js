394c370f220ceff8f48aeed478fd588400e0448c
Tae Lee <taebumlee@gmail.com>
2020-09-09 18:56:33 -0700

    Update simplifyLoopingWithArrowFunctions.js

--- simplifyLoopingWithArrowFunctions.js
+++ simplifyLoopingWithArrowFunctions.js
@@ -5,3 +5,11 @@
 const capitalize = name => name[0].toUpperCase() + name.slice(1);
 const formatUser = name => `${capitalize(name)} is logged in`;
 console.log(formatUser("tae"));
+
+/** callback 함수를 인자로 받는 applyCustomGreeting함수, 그리고 
+ * callback 함수는 다시 capitalize함수의 결과를 인자로 작업을 한다.
+ * 결국 주어진 name문자열의 첫 문자를 대문자로 그걸 다시 callback함수로
+ * 보내서 인사말을 출력
+*/
+const applyCustomGreeting = (name, callback) => callback(capitalize(name));
+console.log(applyCustomGreeting('mark', name => `Oh, hi ${name}!`));
